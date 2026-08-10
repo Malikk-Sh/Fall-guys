8ab1f976e4edf1b7813e6a29cdc0b492b552760f
Малик <93332098+Malikk-Sh@users.noreply.github.com>
2026-08-10 11:38:31 +0300

    Apply canonical migration formatting

--- server/migrations/index.js
+++ server/migrations/index.js
@@ -4,13 +4,7 @@ const authSessions = require('./003_auth_sessions');
 const serverInventory = require('./004_server_inventory');
 const rewardPlatform = require('./005_reward_platform');
 
-const MIGRATIONS = Object.freeze([
-  initial,
-  accountProgress,
-  authSessions,
-  serverInventory,
-  rewardPlatform
-]);
+const MIGRATIONS = Object.freeze([initial, accountProgress, authSessions, serverInventory, rewardPlatform]);
 
 function migrateDatabase(db, { migrations = MIGRATIONS, now = Date.now() } = {}) {
   if (!db) throw new Error('Для миграций нужна открытая база');

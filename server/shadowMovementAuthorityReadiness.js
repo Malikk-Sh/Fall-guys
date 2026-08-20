2d3bdefe6cd0a90b9963cd9caac7a9629182820d
Малик <93332098+Malikk-Sh@users.noreply.github.com>
2026-08-20 23:01:37 +0300

    Format movement authority readiness evaluator

--- server/shadowMovementAuthorityReadiness.js
+++ server/shadowMovementAuthorityReadiness.js
@@ -29,7 +29,9 @@ function evaluateShadowMovementAuthorityReadiness({
   return Object.freeze({
     ready: reasons.length === 0,
     source:
-      reasons.length === 0 ? MOVEMENT_AUTHORITY_SOURCE.SHADOW : MOVEMENT_AUTHORITY_SOURCE.LEGACY,
+      reasons.length === 0
+        ? MOVEMENT_AUTHORITY_SOURCE.SHADOW
+        : MOVEMENT_AUTHORITY_SOURCE.LEGACY,
     reasons: Object.freeze(reasons)
   });
 }
